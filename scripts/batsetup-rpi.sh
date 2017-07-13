d7343f7bed61da032960a32c6f577c83283dc7f5
da7950 <da7950@users.noreply.github.com>
2017-07-13 22:01:38 +0800

    Rewrite
    Raspbian Lite sym links /bin/sh to dash

--- scripts/batsetup-rpi.sh
+++ scripts/batsetup-rpi.sh
@@ -1,25 +1,39 @@
-#! /bin/sh
+#!/bin/sh
 
-# Activate batman-adv
-sudo modprobe batman-adv
+#set -x
 
-# Disable and configure wlan0
-sudo ip link set wlan0 down
-sudo ifconfig wlan0 mtu 1532
-sudo iwconfig wlan0 mode ad-hoc
-sudo iwconfig wlan0 essid KLOG-AD-HOC # Change this to whatever you like
-sudo iwconfig wlan0 ap 02:12:34:56:78:9A
-sudo iwconfig wlan0 channel 1
-sleep 1s
-sudo ip link set wlan0 up
+# node variables
+iface="wlan0"
+node_ip="172.27.0.1/16"
+node_lladr="02:12:34:56:78:9A"
 
-#iwconfig wlan0 essid KLOG-AD-HOC # Uncomment this if you are using a Rasp Pi 1 and have issues with essid not being created
+# cluster variables
+cluster_channel="1"
+cluster_mtu="1532"
+cluster_ssid="KLOG-AD-HOC"
 
-sleep 1s
-sudo batctl if add wlan0
-sleep 1s
-sudo ifconfig bat0 up
-sleep 5s
+exit_error() {
+	echo "${0##*/}: ${1:-"unknown error"}" 1>&2
+	exit 1
+}
 
-# Use different IPv4 addresses for each device
-sudo ifconfig bat0 172.27.0.1/16
+[ "$(id -u)" -ne 0 ] && exit_error "needs root privileges"
+
+# load kernel module
+modprobe batman-adv || exit_error "failed to load batman-adv module"
+
+# configure wireless interface
+ifconfig ${iface} down || exit_error "failed to bring down ${iface}"
+ifconfig ${iface} mtu ${cluster_mtu}
+iwconfig ${iface} ap ${node_lladr}
+iwconfig ${iface} mode ad-hoc
+iwconfig ${iface} channel ${cluster_channel}
+iwconfig ${iface} essid ${cluster_ssid}
+sleep 1s && ifconfig ${iface} up || exit_error "failed to bring up ${iface}"
+
+# configure batman interface
+ifconfig bat0 up || exit_error "failed to bring up batman interface"
+batctl if add ${iface}
+ifconfig bat0 ${node_ip}
+
+exit 0
